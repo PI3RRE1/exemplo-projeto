ea0b0f13b1f9e6bf559027312d602b00c07b208a
PI3RRE1 <pierrelins18@gmail.com>
2024-10-31 15:03:27 -0300

    Adiciona teste automatizado e configuração do pipeline

---
+++ test_example.py
@@ -0,0 +1,13 @@
+import unittest
+
+def soma(a, b):
+    return a + b
+
+class TestSoma(unittest.TestCase):
+    def test_soma(self):
+        self.assertEqual(soma(1, 2), 3)
+        self.assertEqual(soma(-1, 1), 0)
+        self.assertEqual(soma(0, 0), 0)
+
+if __name__ == '__main__':
+    unittest.main()
